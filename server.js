283482599d40bf1d849adceed5dd3159ae9f4991
begumsacak <63175082+begumsacak@users.noreply.github.com>
2020-09-27 20:56:14 -0500

    starter code was fixed & two servers were created

--- server.js
+++ server.js
@@ -6,23 +6,21 @@
 
 var express = require("express");
 var session = require("express-session");
-var exphbs  = require("express-handlebars");
 var compression = require('compression')
 // var bodyParse = require("body-parse");
 
 
 // Requiring passport as we've configured it
-var passport = require("./config/passport");
+//var passport = require("./config/passport");
 
 
 // Sets up the Express App
 
 var app = express();
 var PORT = process.env.PORT || 8080;
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+ 
 // Requiring our models for syncing
-var db = require("./models");
+//var db = require("./models");
 
 app.use(compression())
 
@@ -32,9 +30,9 @@ app.use(express.json());
 
 
 // We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+//app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+//app.use(passport.initialize());
+//app.use(passport.session());
 
 
 
@@ -44,14 +42,14 @@ app.use(express.static("public"));
 
 // Routes
 
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+// require("./routes/html-routes.js")(app);
+// require("./routes/api-routes.js")(app);
 // require("./routes/")(app);
 
 // Syncing our sequelize models and then starting our Express app
 
-db.sequelize.sync({ force: true }).then(function() {
+//db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
-});
+//});
