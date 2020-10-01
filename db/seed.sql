e9592ff34d8c7789c1c6168c10ffb741e127df9c
Clark Kent with glasses <61812163+locutusOO1@users.noreply.github.com>
2020-09-30 21:08:54 -0500

    added user and tv_show tables with db validation, schema file, and seed file

---
+++ db/seed.sql
@@ -0,0 +1,14 @@
+insert into users (userName,password,timeAvailable,createdAt,updatedAt) values ("lokiloki","idontlikethor",5.5,now(),now());
+insert into users (userName,password,timeAvailable,createdAt,updatedAt) values ("supperman","testtest",1.0,now(),now());
+insert into users (userName,password,timeAvailable,createdAt,updatedAt) values ("dinnerman","testtest",0.0,now(),now());
+
+-- select * from users;
+
+insert into tv_shows (name,description,image,runtime,numOfEpisodes,rating,genre,timeBudgeted,timeLogged,tvShowID,createdAt,updatedAt,UserId) 
+values ("Lost","A waste of time.","https://static.episodate.com/images/tv-show/full/4228.jpg",60.0,120,2.5,"Mystery",1.5,27.5,4228,now(),now(),1);
+insert into tv_shows (name,description,image,runtime,numOfEpisodes,rating,genre,timeBudgeted,timeLogged,tvShowID,createdAt,updatedAt,UserId) 
+values ("The Flash","A fun time.","https://static.episodate.com/images/tv-show/full/35624.jpg",60.0,200,9.5,"Super Hero",2.0,45,35624,now(),now(),1);
+insert into tv_shows (name,description,image,runtime,numOfEpisodes,rating,genre,timeBudgeted,timeLogged,tvShowID,createdAt,updatedAt,UserId) 
+values ("The 100","Everything falls apart.","https://static.episodate.com/images/tv-show/full/33514.jpg",60.0,100,8.5,"Future",2.0,70,33514,now(),now(),1);
+
+-- select * from tv_shows;
---
+++ models/tv_show.js
@@ -0,0 +1,75 @@
+// Creating our tv_show model
+module.exports = function(sequelize, DataTypes) {
+    const Tv_show = sequelize.define(
+      "Tv_show",
+      {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            defaultValue: "",
+            allowNull: false
+        },
+        image: {
+            type: DataTypes.STRING,
+            defaultValue: "",
+            allowNull: false
+        },
+        runtime: {
+            type: DataTypes.FLOAT,
+            defaultValue: 0,
+            allowNull: false
+        },
+        numOfEpisodes: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0,
+            allowNull: false
+        },
+        rating: {
+            type: DataTypes.FLOAT,
+            defaultValue: 0.0,
+            allowNull: false
+        },
+        genre: {
+            type: DataTypes.STRING,
+            defaultValue: "",
+            allowNull: false
+        },
+        timeBudgeted: {
+            type: DataTypes.FLOAT,
+            defaultValue: 0.0,
+            allowNull: false,
+            validate: {
+                min: 0,
+                max: 168
+            }
+        },
+        timeLogged: {
+            type: DataTypes.FLOAT,
+            defaultValue: 0.0,
+            allowNull: false,
+            validate: {
+                min: 0
+            }
+        },
+        tvShowID: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0,
+            allowNull: false
+        }
+      }
+    );
+  
+    Tv_show.associate = function(models) {
+        Tv_show.belongsTo(models.User, {
+        foreignKey: {
+          allowNull: false
+        },
+        onDelete: "cascade"
+      });
+    };
+  
+    return Tv_show;
+  };
