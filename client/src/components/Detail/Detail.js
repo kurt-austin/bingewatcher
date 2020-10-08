cbe70bdb74061f3255b42348f99acbd7b699c196
Kurt Austin <kurt.austin@gmail.com>
2020-10-08 12:43:03 -0500

    pushing search app pages for bingewatcher

---
+++ client/src/components/Detail/Detail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Detail() {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container container-fluid">
+                <h1 className="text-center"> BingeWatchers</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Detail;
